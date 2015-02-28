a0b52efe2444785df792030ef73331be62461a31
Nathan Youngman <git@nathany.com>
2015-02-28 11:52:50 -0700

    install hugo if not found OR if not the current version

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -1,8 +1,7 @@
 set -x
 set -e
-if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ]; then
+if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v0.13 ]]; then
   wget https://github.com/spf13/hugo/releases/download/v0.13/hugo_0.13_linux_amd64.tar.gz
   tar xvzf hugo_0.13_linux_amd64.tar.gz
   cp hugo_0.13_linux_amd64/hugo_0.13_linux_amd64 $CIRCLE_BUILD_DIR/bin/hugo
 fi
-if ! [[ `hugo version` =~ v0.13 ]] ; then exit 1; fi
